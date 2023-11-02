41288269cb9649ff0c41c467388d32e5629ff317
matheusflsilva <matheusflsilva801@gmail.com>
2023-11-02 15:34:22 -0300

    Fiz o exercicio 102

---
+++ cursoemvideo/exercicios/ex102.py
@@ -0,0 +1,19 @@
+def fatorial(n, show=False):
+    '''
+    -> Calcula o Fatorial de um número.
+    :param n: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    '''
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            print(' x ' if c > 1 else ' = ', end='')
+        f *= c
+    return f
+
+
+# Programa principal
+print(fatorial(5, show=True))
+help(fatorial)
