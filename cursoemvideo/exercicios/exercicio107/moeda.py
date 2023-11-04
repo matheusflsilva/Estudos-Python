e6e5e30095ec1804a657813c07b1d68d0d26e894
matheusflsilva <matheusflsilva801@gmail.com>
2023-11-04 17:38:37 -0300

    Fiz exercicio 107

---
+++ cursoemvideo/exercicios/exercicio107/moeda.py
@@ -0,0 +1,17 @@
+def aumentar(num, porcen):
+    a = num + (num * porcen / 100)
+    return a
+
+
+def diminuir(num, porcen):
+    d = num - (num * porcen / 100)
+    return d
+
+
+def dobro(num):
+    return num * 2
+
+
+def metade(num):
+    return num / 2
+
